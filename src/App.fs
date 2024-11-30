980db6716dac0b141346b956cc619f9478614b3e
JacobMarineau <jacobmarineaump@gmail.com>
2024-11-30 01:00:57 -0600

    got red cube to render

--- src/App.fs
+++ src/App.fs
@@ -1,14 +1,42 @@
 module App
 
+open Fable.Core.JsInterop
 open Browser.Dom
 
-// Mutable variable to count the number of times we clicked the button
-let mutable count = 0
+let THREE: obj = importAll "three"
 
-// Get a reference to our button and cast the Element to an HTMLButtonElement
-let myButton = document.querySelector(".my-button") :?> Browser.Types.HTMLButtonElement
+let createNew<'T> (ctor: obj) (args: obj list) : 'T =
+    emitJsExpr (ctor, args) "new $0(...$1)"
 
-// Register our listener
-myButton.onclick <- fun _ ->
-    count <- count + 1
-    myButton.innerText <- sprintf "You clicked: %i time(s)" count
+// Create Three.js objects
+let scene: obj = createNew THREE?Scene []
+let camera: obj = createNew THREE?PerspectiveCamera [75.0; window.innerWidth / window.innerHeight; 0.1; 1000.0]
+let renderer: obj = createNew THREE?WebGLRenderer []
+
+renderer?setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer?domElement) |> ignore
+
+// Add a basic red cube for testing
+let testCube: obj =
+    let geometry = createNew THREE?BoxGeometry [1.0; 1.0; 1.0]
+    let material = createNew THREE?MeshBasicMaterial [createObj ["color" ==> "#ff0000"]] // Red cube
+    createNew THREE?Mesh [geometry; material]
+
+scene?add(testCube)
+
+// Position the camera
+camera?position?set(0.0, 0.0, 5.0)
+camera?lookAt(scene?position)
+
+// Lighting (if needed)
+let light: obj = createNew THREE?AmbientLight ["#ffffff"; 1.0]
+scene?add(light)
+
+// Animation loop
+let rec animate () =
+    window.requestAnimationFrame(fun _ -> animate())
+    testCube?rotation?x <- testCube?rotation?x + 0.02
+    testCube?rotation?y <- testCube?rotation?y + 0.02
+    renderer?render(scene, camera) |> ignore
+
+animate ()
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,20 @@
-// Note this only includes basic configuration for development mode.
-// For a more comprehensive configuration check:
-// https://github.com/fable-compiler/webpack-config-template
-
 var path = require("path");
 
 module.exports = {
-    mode: "development",
-    entry: "./src/App.fs.js",
-    output: {
-        path: path.join(__dirname, "./public"),
-        filename: "bundle.js",
+  mode: "development",
+  entry: "./src/App.fs.js",
+  output: {
+    path: path.join(__dirname, "./public"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "public"),
+      publicPath: "/",
     },
-    devServer: {
-        static: {
-            directory: path.resolve(__dirname, "./public"),
-            publicPath: "/",
-        },
-        port: 8080,
-    },
-    module: {
-    }
-}
+    port: 8080,
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, "node_modules")],
+  },
+};
